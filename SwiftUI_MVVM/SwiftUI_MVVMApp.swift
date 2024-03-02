23cd800bcb7d957af41ab432fcf41b403ee37de4
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2024-03-02 10:45:24 +0500

    Initial Commit

---
+++ SwiftUI_MVVM/SwiftUI_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVMApp.swift
+//  SwiftUI_MVVM
+//
+//  Created by A'zamjon Abdumuxtorov on 02/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
